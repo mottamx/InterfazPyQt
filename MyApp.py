a79a1a8a845a1507de237b39314080a5bd9cc4c4
mottamx <cmottaa@gmail.com>
2023-04-04 14:26:32 -0600

    Added Menu, submenu, keyboard shortcut, alt easy access to menu
    Added Menu, submenu, keyboard shortcut, alt easy access to menu

--- MyApp.py
+++ MyApp.py
@@ -16,6 +16,42 @@ class MainWindow(QMainWindow):
         label.setAlignment(Qt.AlignCenter)
         self.setCentralWidget(label) #Be in the middle
 
+        toolbar=QToolBar("Main toolbar")
+        toolbar.setIconSize(QSize(16,16))
+        self.addToolBar(toolbar)
+
+        button_action=QAction(QIcon("bug.png"), "Your button", self)
+        button_action.setStatusTip("This is your button")
+        button_action.triggered.connect(self.onMyToolBarButtonClick)
+        button_action.setCheckable(True)
+        button_action.setShortcut(QKeySequence("Ctrl+p"))
+        toolbar.addAction(button_action)
+
+        toolbar.addSeparator()
+        button_action2=QAction(QIcon("bug.png"), "Your button2", self)
+        button_action2.setStatusTip("This is your button 2")
+        button_action2.triggered.connect(self.onMyToolBarButtonClick)
+        button_action2.setCheckable(True)
+        toolbar.addAction(button_action2)        
+
+        toolbar.addWidget(QLabel("Hello"))
+        toolbar.addWidget(QCheckBox())
+
+        self.setStatusBar(QStatusBar(self))
+
+        menu=self.menuBar()
+        file_menu=menu.addMenu("&File") #Ampersand is letter for Alt menu
+        file_menu.addAction(button_action)
+        file_menu.addSeparator()
+
+        file_submenu = file_menu.addMenu("Submenu")
+        file_submenu.addAction(button_action2)
+
+    def contextMenuEvent(self, event):
+        print("Context event")
+
+    def onMyToolBarButtonClick(self,s):
+        print("click",s)
 
 #Only one of this per application. Pass sys argv
 app=QApplication(sys.argv)
@@ -25,4 +61,3 @@ window.show()
 
 #Start loop
 app.exec_()
-
