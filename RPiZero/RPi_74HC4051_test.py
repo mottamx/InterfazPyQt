a790e77cd0e876b28ac3ebcee8295914484b1662
mottamx <cmottaa@gmail.com>
2023-04-13 12:55:33 -0600

    Fixed to work on a list
    Added channels in a list, loops forward then reversed.

--- RPiZero/RPi_74HC4051_test.py
+++ RPiZero/RPi_74HC4051_test.py
@@ -1,5 +1,4 @@
-#Rpi and 74HC4051 
-#Pinout of RPi Zero from https://es.pinout.xyz/pinout/# 
+
 #This is a test to check if the Multiplexer is correctly displaying outputs (or x_X)
 #Update: Note: The enable pin has to be between GPIOs 0 to 8 bc have pull-ups enabled - the rest have pull-downs enabled
 
@@ -17,66 +16,31 @@ GPIO.setup(22, GPIO.OUT) #S2
 GPIO.setup(2, GPIO.OUT) #Enable, check notes
 GPIO.output(2, GPIO.LOW)
 tiempo = 0.5
+#First channel 000
+        #Second channel 100
+        #Third channel 010
+        #Fourth channel 110
+        #Fifth channel 001
+        #Sixth channel 101
+        #Seventh channel 011
+        #Eigth channel 111
+keys = ["000", "100", "010", "110", "001", "101", "011", "111"]
 while i < 2:
-    # First channel 000
-    print("Y0")
-    GPIO.output(17, GPIO.LOW)
-    GPIO.output(27, GPIO.LOW)
-    GPIO.output(22, GPIO.LOW)
-    time.sleep(tiempo)
-
-    # Second channel 100
-    print("Y1")
-    GPIO.output(17, GPIO.HIGH)
-    GPIO.output(27, GPIO.LOW)
-    GPIO.output(22, GPIO.LOW)
-    time.sleep(tiempo)
-
-    #Third channel 010
-    print("Y2")
-    GPIO.output(17, GPIO.LOW)
-    GPIO.output(27, GPIO.HIGH)
-    GPIO.output(22, GPIO.LOW)
-    time.sleep(tiempo)
-
-    #Fourth channel 110
-    print("Y3")
-    GPIO.output(17, GPIO.HIGH)
-    GPIO.output(27, GPIO.HIGH)
-    GPIO.output(22, GPIO.LOW)
-    time.sleep(tiempo)
-
-    #Fifth channel 001
-    print("Y4")
-    GPIO.output(17, GPIO.LOW)
-    GPIO.output(27, GPIO.LOW)
-    GPIO.output(22, GPIO.HIGH)
-    time.sleep(tiempo)
-
-    #Sixth channel 101
-    print("Y5")
-    GPIO.output(17, GPIO.HIGH)
-    GPIO.output(27, GPIO.LOW)
-    GPIO.output(22, GPIO.HIGH)
-    time.sleep(tiempo)
-
-    #Seventh channel 011
-    print("Y6")
-    GPIO.output(17, GPIO.LOW)
-    GPIO.output(27, GPIO.HIGH)
-    GPIO.output(22, GPIO.HIGH)
-    time.sleep(tiempo)
-
-    #Eigth channel 111
-    print("Y7")
-    GPIO.output(17, GPIO.HIGH)
-    GPIO.output(27, GPIO.HIGH)
-    GPIO.output(22, GPIO.HIGH)
-    time.sleep(tiempo)
-
+    for keyStroke in keys:
+        print("Key", keyStroke)
+        GPIO.output(17, int(keyStroke[0]))
+        GPIO.output(27, int(keyStroke[1]))
+        GPIO.output(22, int(keyStroke[2]))
+        time.sleep(tiempo)
+    #Now backwards    
+    for keyStroke in reversed(keys):
+        print("Key", keyStroke)
+        GPIO.output(17, int(keyStroke[0]))
+        GPIO.output(27, int(keyStroke[1]))
+        GPIO.output(22, int(keyStroke[2]))
+        time.sleep(tiempo)
     print("Everything off")
     i += 1 #Repeat i times
-
 print("End of while") 
 
 GPIO.output(2, GPIO.HIGH) #Disable EN pin
